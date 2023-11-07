f56b43b3a67ee6eb428e50be37a9d95d5885a488
b1809128 <nghoquochuy0902@gmail.com>
2023-11-07 21:32:48 +0700

    update draw info

--- lp_image.py
+++ lp_image.py
@@ -1,7 +1,8 @@
+from datetime import datetime
 from PIL import Image
 import cv2
 import torch
-import math 
+import math
 import function.utils_rotate as utils_rotate
 from IPython.display import display
 import os
@@ -13,8 +14,10 @@ ap = argparse.ArgumentParser()
 ap.add_argument('-i', '--image', required=True, help='path to input image')
 args = ap.parse_args()
 
-yolo_LP_detect = torch.hub.load('yolov5', 'custom', path='model/LP_detector.pt', force_reload=True, source='local')
-yolo_license_plate = torch.hub.load('yolov5', 'custom', path='model/LP_ocr.pt', force_reload=True, source='local')
+yolo_LP_detect = torch.hub.load(
+    'yolov5', 'custom', path='model/LP_detector.pt', force_reload=True, source='local')
+yolo_license_plate = torch.hub.load(
+    'yolov5', 'custom', path='model/LP_ocr.pt', force_reload=True, source='local')
 yolo_license_plate.conf = 0.60
 
 img = cv2.imread(args.image)
@@ -23,29 +26,52 @@ plates = yolo_LP_detect(img, size=640)
 plates = yolo_LP_detect(img, size=640)
 list_plates = plates.pandas().xyxy[0].values.tolist()
 list_read_plates = set()
+
+now = datetime.now()
+dt_string = now.strftime("%d/%m/%Y %H:%M:%S")
+
 if len(list_plates) == 0:
-    lp = helper.read_plate(yolo_license_plate,img)
+    lp = helper.read_plate(yolo_license_plate, img)
     if lp != "unknown":
-        cv2.putText(img, lp, (7, 70), cv2.FONT_HERSHEY_SIMPLEX, 0.9, (36,255,12), 2)
+        cv2.putText(img, lp, (7, 70), cv2.FONT_HERSHEY_SIMPLEX,
+                    0.9, (36, 255, 12), 2, 2)
         list_read_plates.add(lp)
 else:
     for plate in list_plates:
         flag = 0
-        x = int(plate[0]) # xmin
-        y = int(plate[1]) # ymin
-        w = int(plate[2] - plate[0]) # xmax - xmin
-        h = int(plate[3] - plate[1]) # ymax - ymin  
+        x = int(plate[0])  # xmin
+        y = int(plate[1])  # ymin
+        w = int(plate[2] - plate[0])  # xmax - xmin
+        h = int(plate[3] - plate[1])  # ymax - ymin
         crop_img = img[y:y+h, x:x+w]
-        cv2.rectangle(img, (int(plate[0]),int(plate[1])), (int(plate[2]),int(plate[3])), color = (0,0,225), thickness = 2)
+        cv2.rectangle(img, (int(plate[0]), int(plate[1])), (int(
+            plate[2]), int(plate[3])), color=(0, 0, 225), thickness=2)
         cv2.imwrite("crop.jpg", crop_img)
         rc_image = cv2.imread("crop.jpg")
         lp = ""
-        for cc in range(0,2):
-            for ct in range(0,2):
-                lp = helper.read_plate(yolo_license_plate, utils_rotate.deskew(crop_img, cc, ct))
+        for cc in range(0, 2):
+            for ct in range(0, 2):
+                lp = helper.read_plate(
+                    yolo_license_plate, utils_rotate.deskew(crop_img, cc, ct))
                 if lp != "unknown":
                     list_read_plates.add(lp)
-                    cv2.putText(img, lp, (int(plate[0]), int(plate[1]-10)), cv2.FONT_HERSHEY_SIMPLEX, 0.9, (36,255,12), 2)
+                    (text_width, text_height) = cv2.getTextSize(
+                        lp, cv2.FONT_HERSHEY_SIMPLEX, fontScale=0.5, thickness=1)[0]
+                    text_offset_x = int(plate[0])
+                    text_offset_y = int(plate[1]-10)
+                    # make the coords of the box with a small padding of two pixels
+                    box_coords = ((text_offset_x, text_offset_y), (text_offset_x +
+                                  text_width + 60, text_offset_y-30 - text_height - 2))
+                    cv2.rectangle(
+                        img, box_coords[0], box_coords[1], (0, 0, 0), cv2.FILLED)
+                    cv2.putText(img, "Detect: Car", (int(plate[0]), int(
+                        plate[1]-40)), cv2.FONT_HERSHEY_SIMPLEX, 0.3, (255, 255, 255), 1)
+                    cv2.putText(img, "Status: Unknow Data", (int(plate[0]), int(
+                        plate[1]-30)), cv2.FONT_HERSHEY_SIMPLEX, 0.3, (255, 255, 255), 1)
+                    cv2.putText(img, "Time: "+dt_string, (int(plate[0]), int(
+                        plate[1]-20)), cv2.FONT_HERSHEY_SIMPLEX, 0.3, (255, 255, 255), 1)
+                    cv2.putText(img, "Plate: "+lp, (int(plate[0]), int(
+                        plate[1]-10)), cv2.FONT_HERSHEY_SIMPLEX, 0.3, (255, 255, 255), 1)
                     flag = 1
                     break
             if flag == 1:
--- webcam.py
+++ webcam.py
@@ -12,6 +12,10 @@ import function.helper as helper
 import pandas as pd
 import csv
 
+now = datetime.now()
+dt_string = now.strftime("%d/%m/%Y %H:%M:%S")
+# dateStr = today.strftime("%d/%m/%Y")
+
 # load model
 yolo_LP_detect = torch.hub.load(
     'yolov5', 'custom', path='model/LP_detector_nano_61.pt', force_reload=True, source='local')
@@ -23,7 +27,18 @@ prev_frame_time = 0
 new_frame_time = 0
 
 vid = cv2.VideoCapture(0)
-# vid = cv2.VideoCapture("1.mp4")
+# vid = cv2.VideoCapture("./test_image/test.mp4")
+
+
+def check_plate(plate):
+    ub_list_plate = ["65A19777", "65A29999"]
+    for i in ub_list_plate:
+        if (i == plate):
+            return 1
+        else:
+            return 0
+
+
 plate_text = []
 while (True):
     ret, frame = vid.read()
@@ -50,18 +65,39 @@ while (True):
                     yolo_license_plate, utils_rotate.deskew(crop_img, cc, ct))
                 if lp != "unknown":
                     list_read_plates.add(lp)
-                    cv2.putText(frame, lp, (int(plate[0]), int(
-                        plate[1]-10)), cv2.FONT_HERSHEY_SIMPLEX, 0.9, (36, 255, 12), 2)
+                    (text_width, text_height) = cv2.getTextSize(
+                        lp, cv2.FONT_HERSHEY_SIMPLEX, fontScale=0.5, thickness=1)[0]
+                    text_offset_x = int(plate[0])
+                    text_offset_y = int(plate[1]-10)
+                    # make the coords of the box with a small padding of two pixels
+                    box_coords = ((text_offset_x, text_offset_y), (text_offset_x +
+                                  text_width + 60, text_offset_y-30 - text_height - 2))
+                    cv2.rectangle(
+                        frame, box_coords[0], box_coords[1], (0, 0, 0), cv2.FILLED)
+                    cv2.putText(frame, "Detect: Car", (int(plate[0]), int(
+                        plate[1]-40)), cv2.FONT_HERSHEY_SIMPLEX, 0.3, (255, 255, 255), 1)
+                    # if (check_plate(list_read_plates) == 1):
+                    #     cv2.putText(frame, "Status: Xe co quan UBND", (int(plate[0]), int(
+                    #         plate[1]-30)), cv2.FONT_HERSHEY_SIMPLEX, 0.3, (255, 255, 255), 1)
+                    # else:
+                    #     cv2.putText(frame, "Status: Unknow Data", (int(plate[0]), int(
+                    #         plate[1]-30)), cv2.FONT_HERSHEY_SIMPLEX, 0.3, (255, 255, 255), 1)
+                    cv2.putText(frame, "Status: Xe co quan UBND", (int(plate[0]), int(
+                        plate[1]-30)), cv2.FONT_HERSHEY_SIMPLEX, 0.3, (255, 255, 255), 1)
+                    cv2.putText(frame, "Time: "+dt_string, (int(plate[0]), int(
+                        plate[1]-20)), cv2.FONT_HERSHEY_SIMPLEX, 0.3, (255, 255, 255), 1)
+                    cv2.putText(frame, "Plate: "+lp, (int(plate[0]), int(
+                        plate[1]-10)), cv2.FONT_HERSHEY_SIMPLEX, 0.3, (255, 255, 255), 1)
                     flag = 1
                     break
             if flag == 1:
                 break
-    new_frame_time = time.time()
-    fps = 1/(new_frame_time-prev_frame_time)
-    prev_frame_time = new_frame_time
-    fps = int(fps)
-    cv2.putText(frame, str(fps), (7, 70), cv2.FONT_HERSHEY_SIMPLEX,
-                3, (100, 255, 0), 3, cv2.LINE_AA)
+    # new_frame_time = time.time()
+    # fps = 1/(new_frame_time-prev_frame_time)
+    # prev_frame_time = new_frame_time
+    # fps = int(fps)
+    # cv2.putText(frame, str(fps), (7, 70), cv2.FONT_HERSHEY_SIMPLEX,
+    #             3, (100, 255, 0), 3, cv2.LINE_AA)
     cv2.imshow('frame', frame)
     plate_text += list_read_plates
     if cv2.waitKey(1) & 0xFF == ord('q'):
@@ -69,20 +105,6 @@ while (True):
 
 vid.release()
 cv2.destroyAllWindows()
-
-
-def check_plate(plate):
-    ub_list_plate = ["65A19777", "65A29999"]
-    for i in ub_list_plate:
-        if (i == plate):
-            return 1
-        else:
-            return 0
-
-
-now = datetime.now()
-dt_string = now.strftime("%d/%m/%Y %H:%M:%S")
-# dateStr = today.strftime("%d/%m/%Y")
 
 
 def write_csv_plate(dataw, element):
@@ -96,6 +118,7 @@ def write_csv_plate(dataw, element):
         # writer.writerow([dataw,"14:54 29-10-2023",element])
 
 # from openpyxl import load_workbook
+
 
 def write_excel_plate(dataw, element):
     marks_data = pd.DataFrame(
@@ -118,14 +141,14 @@ def write_text_plate(dataw, time, note):
 
 
 data = ""
-for plt in plate_text:
-    # print(check_plate(plt))
-    if (check_plate(plt) == 1):
-        #    write_text_plate(plt,"","Co quan UBND")
-        write_csv_plate(plt, "Co quan UBND")
-    else:
-        #    write_text_plate(plt,"","Khong co du lieu")
-        write_csv_plate(plt, "Khong co du lieu")
+# for plt in plate_text:
+#     # print(check_plate(plt))
+#     if (check_plate(plt) == 1):
+#         #    write_text_plate(plt,"","Co quan UBND")
+#         write_csv_plate(plt, "Co quan UBND")
+#     else:
+#         #    write_text_plate(plt,"","Khong co du lieu")
+#         write_csv_plate(plt, "Khong co du lieu")
 
 
 print("============================")
